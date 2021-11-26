8d986f3fa0e7cb8d02eb92f9bde56180c427dce3
Joe Shepherd <joe.shepherd@runbox.com>
2021-11-26 12:24:40 +0000

    Added call heat per zone, confirmed working as sum() in Grafana.

--- evohome.py
+++ evohome.py
@@ -9,42 +9,68 @@ print("Starting")
 username = os.environ['EH-USERNAME']
 password = os.environ['EH-PASSWORD']
 
+# influxdb_host = 'influxdb_host'
+influxdb_host = 'localhost'
+db = "EH-TEMPS"
+hotwater_setpoint = 0.0 # Cannot be pulled from API to manually set
+poll_interval = 300.0
+
 starttime = time.time()
 if __name__ == "__main__":
     all_ok = True
     # Connect to Influxdb
-    client = InfluxDBClient(host='influxdb', port=8086)
-    print("Connected to InfluxDB")
-    client.create_database("EH-TEMPS")
-    print("Database EH-TEMPS exists")
+    print("Connecting to InfluxDB")
+    client = InfluxDBClient(host=influxdb_host, port=8086)
+    print(f"Connected. Checking existence of db {db}.")
+    client.create_database(db)
+    print(f"Database {db} found (or created). Getting DB properties.")
     client.get_list_database()
-    print("Initialising evohome API")
-    try:
-        eclient = EvohomeClient({username}, {password})
-    except AuthenticationError as e:
-        print("API overload error - will trsleeping 5 mins before container restart")
-        time.sleep(300)
-        print("Sleep slept")
-        all_ok = False
-
+    # print("Initialising evohome API")
     while all_ok:
+        # Request evohome temperatures
+        print("Collect and store evohome temperatures")
         try:
-            # Request evohome temperatures
-            print("Collect and store evohome temperatures")
-            eclient = EvohomeClient({username}, {password})
             temps_data = list(eclient.temperatures())
-            # print(temps_data)
+
+        except NameError:
+            print("Client API not yet initialised - initialising...")
+            try:
+                eclient = EvohomeClient({username}, {password})
+            except AuthenticationError as e:
+                if "invalid_grant" in e:
+                    print("invalid_grant when authenticating - check your credentials")
+                    all_ok = False
+                else:
+                    if "attempt_limit_exceeded" in e:
+                        print("attempt_limit_exceeded - will try sleeping 5 mins before container restart")
+                    else:
+                        print(e)
+                        print("API overload error - will try sleeping 5 mins before container restart")
+                    time.sleep(300)
+                all_ok = False
+            else:
+                print("Init successful")
+                continue
+
+
+        else: # We managed to get the temps from the API
             for device in temps_data:
+
                 # Hot Water has null strings for name and setpoint so manually add them
                 if device['thermostat'] == "DOMESTIC_HOT_WATER":
-                    device['setpoint'] = 55.0
+                    device['setpoint'] = hotwater_setpoint
                     device['name'] = "Hot Water"
+                    device['call_heat'] = 0
+                # Manually add in a flag for call_heat
+                device['call_heat'] = 1 if device['setpoint'] > device['temp'] else 0
+                #TODO: Need to figure out how to deal with hot water call - use schedule
                 print([{"measurement":"Temperatures","fields":device}])
-                client.write_points([{"measurement":"Temperatures","fields":device}], database='EH-TEMPS')
+                client.write_points([{"measurement":"Temperatures","fields":device}], database=db)
+
 
             # Collect and store OH temperatures
-            print("Collect and store OH temperatures")
             if "OW" in os.environ:
+                print("Collect and store outside weather temperatures")
                 API_key = os.environ['OW-API-KEY']
                 city_name = os.environ['OW-CITY']
                 base_url = "http://api.openweathermap.org/data/2.5/weather?"
@@ -52,15 +78,15 @@ if __name__ == "__main__":
                 weather_data = requests.get(Final_url).json()
                 temp = weather_data['main']['temp']
                 print([{"measurement":"ext-Temperatures","fields":{'ext-temp': temp}}])
-                client.write_points([{"measurement":"ext-Temperatures","fields":{'ext-temp': temp}}], database='EH-TEMPS')
+                client.write_points([{"measurement":"ext-Temperatures","fields":{'ext-temp': temp}}], database=db)
 
             # Inform Healthchecks.io
             if "HEALTHCHECKS-IO" in os.environ:
                 healthchecks = os.environ['HEALTHCHECKS-IO']
                 requests.get(healthchecks)
 
-        except ConnectionError as e:
-            print("No Database Connection")
-        except AuthenticationError as e:
-            print("API overload error - sleeping 5 mins before retry")
-        time.sleep(300.0 - ((time.time() - starttime) % 300.0))
+        # except ConnectionError as e:
+        #     print("No Database Connection")
+        # except AuthenticationError as e:
+        #     print("API overload error - sleeping 5 mins before retry")
+        time.sleep(poll_interval - ((time.time() - starttime) % poll_interval))
