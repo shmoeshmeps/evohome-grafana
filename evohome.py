f25caa45db30682cfe0d3706f6a0b104b85292d6
Joe Shepherd <joe.shepherd@runbox.com>
2021-11-26 16:26:48 +0000

    Updated evohome with sample hw schedule and global boiler indicator

--- evohome.py
+++ evohome.py
@@ -4,6 +4,7 @@ from requests.exceptions import ConnectionError
 import time
 import os
 import requests
+import datetime
 
 print("Starting")
 username = os.environ['EH-USERNAME']
@@ -12,8 +13,30 @@ password = os.environ['EH-PASSWORD']
 # influxdb_host = 'influxdb_host'
 influxdb_host = 'localhost'
 db = "EH-TEMPS"
-hotwater_setpoint = 0.0 # Cannot be pulled from API to manually set
+hotwater_setpoint_max = 55.0 # Cannot be pulled from API to manually set
 poll_interval = 300.0
+
+# Basic hot water manual schedule for setpoints
+
+def set_hw_windows():
+    """
+    A simple way to manually set the hot water activation windows within the script for now.
+    :return:
+    """
+    hw_windows = [
+        {
+            'startwin' : datetime.datetime.now().replace(hour=6),
+            'endwin' : datetime.datetime.now().replace(hour=8),
+        },
+        {
+            'startwin': datetime.datetime.now().replace(hour=16,minute=30),
+            'endwin': datetime.datetime.now().replace(hour=18,minute=30),
+        },
+    ]
+    return hw_windows
+
+
+
 
 starttime = time.time()
 if __name__ == "__main__":
@@ -54,18 +77,26 @@ if __name__ == "__main__":
 
 
         else: # We managed to get the temps from the API
+            now = datetime.datetime.now()
+            hw_windows = set_hw_windows() # Updates windows to be the hours of today
+            hotwater_setpoint = 0.0
+            for hw_window in hw_windows:
+                if now > hw_window['startwin'] and now < hw_window['endwin']:
+                    hotwater_setpoint = hotwater_setpoint_max
             for device in temps_data:
 
                 # Hot Water has null strings for name and setpoint so manually add them
                 if device['thermostat'] == "DOMESTIC_HOT_WATER":
                     device['setpoint'] = hotwater_setpoint
                     device['name'] = "Hot Water"
-                    device['call_heat'] = 0
                 # Manually add in a flag for call_heat
                 device['call_heat'] = 1 if device['setpoint'] > device['temp'] else 0
                 #TODO: Need to figure out how to deal with hot water call - use schedule
                 print([{"measurement":"Temperatures","fields":device}])
                 client.write_points([{"measurement":"Temperatures","fields":device}], database=db)
+            global_heat = 1 if any([x['call_heat'] for x in temps_data]) else 0
+            client.write_points([{"measurement":"Global","fields":{'call_heat':global_heat}}], database=db)
+            print(f"Global Heat: {global_heat}")
 
 
             # Collect and store OH temperatures
@@ -90,3 +121,9 @@ if __name__ == "__main__":
         # except AuthenticationError as e:
         #     print("API overload error - sleeping 5 mins before retry")
         time.sleep(poll_interval - ((time.time() - starttime) % poll_interval))
+
+"""
+If a TRV reaches it's setpoint, does it fully close or partially close?
+Does it then stop the boiler call?
+How does the boiler overrun thing play into this?
+"""
